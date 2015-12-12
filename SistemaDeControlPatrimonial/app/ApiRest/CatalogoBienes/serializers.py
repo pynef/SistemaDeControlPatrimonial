a869d38eface49bbf83ce7e2b552699640ace882
nefi <nefinef@gmail.com>
2015-12-11 23:16:51 -0500

    correcion de nombre de las aplicaciones

--- SistemaDeControlPatrimonial/app/ApiRest/CatalogoBienes/serializers.py
+++ SistemaDeControlPatrimonial/app/ApiRest/CatalogoBienes/serializers.py
@@ -2,3 +2,13 @@ from django.forms import widgets
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer
+from SistemaDeControlPatrimonial.app.CatalogoBienes.models import *
+
+class GrupoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Grupo
+        fields = ('id', 'institucion', 'nombre', 'descripcion')
+        read_only_fields = ('created_at', 'updated_at',)
+        
+        def create(self, validated_data):
+            return Grupo.objects.create(**validated_data)
--- SistemaDeControlPatrimonial/app/ApiRest/CatalogoBienes/views.py
+++ SistemaDeControlPatrimonial/app/ApiRest/CatalogoBienes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render, get_object_or_404
+# from django.contrib.auth.models import User
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
 
-# Create your views here.
+from rest_framework import generics, permissions
+from SistemaDeControlPatrimonial.app.CatalogoBienes.models import Grupo, Clase, TipoCatalogoBien, CatalogoBien
+from serializers import GrupoSerializer
+
+
+class GrupoList(generics.ListAPIView):
+    model = Grupo
+    serializer_class = GrupoSerializer
+    permission_classes = [
+        permissions.AllowAny
+    ]
